cfabe7f7481277ddc2078167311cf98f53ae6992
carabria <studyingrhystic@gmail.com>
2024-05-22 23:12:04 -0400

    readme and dummy data added

--- insert_data.sh
+++ insert_data.sh
@@ -2,14 +2,14 @@
 PSQL="psql --username=postgres --dbname=smtdle -t --no-align -c"
 TRUNCATE=$($PSQL"TRUNCATE demons RESTART IDENTITY")
 echo $TRUNCATE
-cat data.csv | while IFS=',' read NAME ALIGNMENT MYTHOLOGY FIRST_APPEARANCE AFFINITY_ONE AFFINITY_TWO RACE
+cat data.csv | while IFS=',' read NAME ALIGNMENT_ONE ALIGNMENT_TWO MYTHOLOGY AFFINITY_ONE AFFINITY_TWO RACE
 do
 if [[ $NAME != name ]]
 then
     DEMON_ID=$($PSQL "SELECT demon_id FROM demons WHERE name='$NAME'")
     if [[ -z $DEMON_ID ]]
     then
-        INSERT_DEMON_RESULT=$($PSQL "INSERT INTO demons(name, alignment, mythology, first_appearance, affinity_one, affinity_two, race) VALUES('$NAME', '$ALIGNMENT', '$MYTHOLOGY', '$FIRST_APPEARANCE', '$AFFINITY_ONE', '$AFFINITY_TWO', '$RACE')")
+        INSERT_DEMON_RESULT=$($PSQL "INSERT INTO demons(name, alignment_one, alignment_two, mythology, affinity_one, affinity_two, race) VALUES('$NAME', '$ALIGNMENT_ONE', '$ALIGNMENT_TWO', '$MYTHOLOGY', '$AFFINITY_ONE', '$AFFINITY_TWO', '$RACE')")
         echo $INSERT_DEMON_RESULT
         DEMON_ID=$($PSQL "SELECT demon_id FROM demons WHERE name='$NAME'")
     fi
---
+++ readme.md
@@ -0,0 +1,1 @@
+Demons from SMT V will be included. In the event of multiple ailments, highest affinity will be selected. In some cases, resistances are selected over +1 affinities.
